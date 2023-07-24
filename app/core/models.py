214677f9ad50909c84a24377ddb95b0fac81f3ad
matteo <avignimatteo@gmail.com>
2023-07-23 23:25:22 -0400

    add user and superuser

--- app/core/models.py
+++ app/core/models.py
@@ -10,9 +10,25 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create and return a new superuser.
+        
+        admin@example.com
+        4Dm!n123
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
